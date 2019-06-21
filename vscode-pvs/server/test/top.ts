54aad8131348d8735fa7c34c7bc63100f5fbbdb1
Paolo M. Masci <paolo.m.masci@nasa.gov>
2019-06-21 17:07:14 -0400

    more tests for pvsProcess

--- vscode-pvs/server/test/top.ts
+++ vscode-pvs/server/test/top.ts
@@ -1,5 +1,5 @@
 import { PvsProcess } from "./server/pvsProcess";
-import { PvsResponseType, PvsVersionInfoResponseType, JsonType, ProofNodeType, ProofObjectType } from "./server/common/serverInterface";
+import { PvsResponseType, PvsVersionInfoResponseType, JsonType, ProofNodeType, ProofObjectType, PvsListProofStrategiesResponseType, StrategyDescriptor, PvsFindDeclarationResponseType } from "./server/common/serverInterface";
 import * as fsUtils from "./server/common/fsUtils";
 import * as test from "./test-costants";
 import * as path from 'path';
@@ -26,8 +26,8 @@ describe("PvsProcess", async () => {
 
 	it("constructor can create a pvs process", async () => {
 		const proc: PvsProcess = new PvsProcess({ pvsPath });
-		const success = await proc.pvs();
-		expect(success).toBeTruthy();
+		const success: boolean = await proc.pvs();
+		expect(success).toBeDefined();
 		const versionInfo: PvsResponseType = await proc.getPvsVersionInfo();
 		expect(versionInfo).toBeDefined();
 		expect(versionInfo.res).toBeDefined();
@@ -75,6 +75,18 @@ describe("PvsProcess", async () => {
 		expect(pid).toBeDefined();
 	});
 
+	it("kill() can kill a pvs process", async () => {
+		const proc: PvsProcess = new PvsProcess({ pvsPath });
+		await proc.pvs();
+		const pid: string = await proc.getProcessID();
+		log(`Process ID: ${pid}`);
+		expect(pid).toBeDefined();
+		const killed_pid: string = await proc.kill();
+		log(`Killed process ID: ${killed_pid}`);
+		expect(killed_pid).toBeDefined();
+		expect(killed_pid).toEqual(pid);
+	});
+
 	it("getPvsPath can provide the path to the pvs executable", async () => {
 		const proc: PvsProcess = new PvsProcess({ pvsPath });
 		await proc.pvs();
@@ -115,4 +127,34 @@ describe("PvsProcess", async () => {
 		expect(version.res.lispVersion).toBeDefined();
 	});
 
+	it("listProofStrategies() can print proof strategies", async () => {
+		const proc: PvsProcess = new PvsProcess({ pvsPath });
+		await proc.pvs();
+		const strategies: PvsListProofStrategiesResponseType = await proc.listProofStrategies();
+		expect(strategies).toBeDefined();
+		expect(strategies.res).toBeDefined();
+		log(strategies.res);
+		strategies.res.forEach((strategy: StrategyDescriptor) => {
+			expect(strategy.description).toBeDefined();
+			expect(strategy.name).toBeDefined();
+		});
+	});
+
+	it("findDeclaration() can find declarations from the prelude", async () => {
+		const proc: PvsProcess = new PvsProcess({ pvsPath });
+		await proc.pvs();
+		const declarations: PvsFindDeclarationResponseType = await proc.findDeclaration("real");
+		expect(declarations).toBeDefined();
+		expect(declarations.res).toBeDefined();
+		log(declarations.res);
+		expect(Object.keys(declarations.res).length).toEqual(1);
+		Object.keys(declarations.res).forEach((key: string) => {
+			// key is in the form theoryName.symbolName
+			expect(declarations.res[key].symbolDeclaration).toBeDefined();
+			expect(declarations.res[key].symbolDeclarationFile).toBeDefined();
+			expect(declarations.res[key].symbolDeclarationRange).toBeDefined();
+			expect(declarations.res[key].symbolName).toBeDefined();
+			expect(declarations.res[key].symbolTheory).toBeDefined();
+		});
+	});
 });
