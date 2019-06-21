edc9875b85c3e402a7127175e492029135b0a84c
Paolo M. Masci <paolo.m.masci@nasa.gov>
2019-06-21 14:23:28 -0400

    added new test cases for pvsProcess

--- vscode-pvs/server/test/top.ts
+++ vscode-pvs/server/test/top.ts
@@ -1,18 +1,47 @@
 import { PvsProcess } from "./server/pvsProcess";
 import { PvsResponseType } from "./server/common/serverInterface";
+import * as fsUtils from "./server/common/fsUtils";
 
-describe("PvsServer", async () => {
-	const pvsPath: string = "/Users/pmasci/Work/pvs-snapshots/pvs-7.0-851";
-	it("should be able to create a pvs process", async () => {
-		let proc = new PvsProcess({
-			pvsPath,
-			pvsContextFolder: "./"
-		});
+const VERBOSE: boolean = false;
+function log (data: any) {
+	if (VERBOSE) {
+		console.log("\n", data);
+	}
+}
+
+describe("PvsProcess", async () => {
+	log("PvsProcess");
+	const pvsPath: string = "~/Work/pvs-snapshots/pvs-7.0-851/";
+
+	it("constructor should be able to create a pvs process", async () => {
+		const proc = new PvsProcess({ pvsPath });
 		const success = await proc.pvs();
 		expect(success).toBeTruthy();
 		const versionInfo: PvsResponseType = await proc.pvsVersionInformation();
 		expect(versionInfo).not.toBeNull();
 		expect(versionInfo.res).not.toBeNull();
-		console.log(versionInfo.res);
+		log(versionInfo.res);
+	});
+
+	it("constructor should be able to set context", async () => {
+		const pvsContextFolder: string = "~/Work/";
+		const proc = new PvsProcess({ pvsPath, pvsContextFolder });
+		await proc.pvs();
+		const ctx: string = await proc.getContextFolder();
+		log(`Current context: ${ctx}`);
+		expect(ctx).toEqual(pvsContextFolder);
+	});
+
+	it("changeContext() should be able to change context", async () => {
+		const proc = new PvsProcess({ pvsPath });
+		await proc.pvs();
+		const ctx: string = await proc.getContextFolder();
+		log(`Current context: ${ctx}`);
+		await proc.changeContext("..");
+		const new_ctx: string = await proc.getContextFolder();
+		log(`New context: ${new_ctx}`);
+		expect(new_ctx).not.toEqual(ctx);
+		const sup: string = `${ctx.split("/").slice(0, -2).join("/")}/`;
+		expect(new_ctx).toEqual(sup);
 	});
 });
