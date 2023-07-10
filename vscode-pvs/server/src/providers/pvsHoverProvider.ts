9379a774a0ff15de4321286cc31ec6d1034c3ebb
Paolo M. Masci <paolo.m.masci@nasa.gov>
2023-07-10 14:42:49 -0400

    added hover tooltip for TCCs, to view the expression that triggered the tcc

--- vscode-pvs/server/src/providers/pvsHoverProvider.ts
+++ vscode-pvs/server/src/providers/pvsHoverProvider.ts
@@ -37,12 +37,11 @@
  **/
 
 import { PvsDefinition } from '../common/serverInterface';
-import { TextDocument, Position, CancellationToken, Hover, Range } from 'vscode-languageserver';
+import { Position, CancellationToken, Hover } from 'vscode-languageserver';
 import { MarkedString } from 'vscode-languageserver-types';
 import { PvsDefinitionProvider } from "./pvsDefinitionProvider";
 import * as language from "../common/languageKeywords";
 import * as fsUtils from '../common/fsUtils';
-import * as utils from '../common/languageUtils';
 
 export class PvsHoverProvider {
 	/**`
@@ -79,15 +78,61 @@ export class PvsHoverProvider {
 			if (isComment) { return null; }
 			// else, not a comment
 			const ans: { symbolName: string, definitions: PvsDefinition[] } = await this.definitionProvider.getDefinition(desc);
-			if (ans) {
-				const definitions: PvsDefinition[] = ans.definitions;
-				if (definitions && definitions.length > 0) {
+			// check if this is a TCC, if so show the expression that generated the TCC
+			const regex: RegExp = new RegExp(/.+_TCC.*/g);
+			if (regex.test(ans?.symbolName)) {
+				const formulaName: string = ans?.symbolName || "";
+				const formulaIndex: number = desc.txt.indexOf(formulaName);
+				const lines: string[] = desc.txt.substring(0, formulaIndex + formulaName.length).split("\n");
+				// find the block of lines with the comments for the tcc
+				// the block starts with an empty line and ends with the tcc name
+				let upperIndex: number = -1;
+				for (let i = 0; i < lines.length && upperIndex < 0; i++) {
+					if (lines[lines.length - i - 1].trim().length === 0) {
+						upperIndex = lines.length - i - 1;
+					}
+				}
+				if (upperIndex > 0) {
+					const txt: string = lines.slice(upperIndex).join("\n");
+					// group 1 is the line-column information
+					// group 2 is the line (1-based)
+					// group 3 is the column (1-based)
+					// const regex: RegExp = new RegExp(/\(at (line (\d+), column (\d+))\)/g);
+					// group 4 is optional, and corresponds to the expression that generated the tcc -- this grop is captured only if the expression is on one line
+					const regex: RegExp = new RegExp(/\(at (line (\d+), column (\d+))\)(?:[.*\s%]+for[\s%]+(.*))?/g);
+					const matchInfo: RegExpMatchArray = regex.exec(txt);
+					if (matchInfo?.length > 4) {
+						const line: number = +matchInfo[2];
+						const col: number = +matchInfo[3];
+						const expression: string = matchInfo[4];
+						const fname: string = fsUtils.desc2fname({ ...fsUtils.fname2desc(desc.uri), fileExtension: ".pvs" });
+						const tccType: string = txt.substring(txt.indexOf("%") + 1, txt.indexOf("TCC"));
+						const pvsFile: MarkedString = `[${fsUtils.getFileName(fname, { keepExtension: true })} (Ln ${line}, Col ${col})](file://${fname}#L${line}:${col})`
+						contents.push(`${tccType} TCC generated ${expression?.length ? ` for \`${expression}\`` : ""}`);
+						contents.push(`at ${pvsFile}`);
+						const pvsFileContent: string = await fsUtils.readFile(fname);
+						const pvsFileLines: string[] = pvsFileContent?.split("\n");
+						if (pvsFileLines?.length > line - 1) {
+							const pvsExpression: string = pvsFileLines[line - 1];
+							if (pvsExpression) {
+								contents.push({
+									language: "pvs",
+									value: pvsExpression.replace(/\t/g, " ") + `\n${col ? " ".repeat(col) : ""}${"^".repeat(expression?.length || 1)}`
+								});
+							}
+						}
+					}
+				}
+			} else {
+				// else, try to ask PVS for a definitions
+				if (ans?.symbolName && ans?.definitions?.length) {
+					const definitions: PvsDefinition[] = ans?.definitions;
 					// give preference to definitions in the same file
 					const sameTheoryDefs: PvsDefinition[] = definitions.filter((def: PvsDefinition) => {
 						return def.symbolDeclarationFile === desc.uri;
 					});
-					const def: PvsDefinition = (sameTheoryDefs && sameTheoryDefs.length > 0) ? sameTheoryDefs[0] : definitions[0];
-					if (def.error) {
+					const def: PvsDefinition = (sameTheoryDefs && sameTheoryDefs?.length > 0) ? sameTheoryDefs[0] : definitions[0];
+					if (def?.error) {
 						// errors are shown alone in the hover to avoid cluttering
 						contents.push(def.error.message);
 					} else {
@@ -97,37 +142,37 @@ export class PvsHoverProvider {
 								language: "pvs"
 							});
 						}
-						if (definitions.length > 1) {
+						if (definitions?.length > 1) {
 							// const uri: string = (document.uri.startsWith("file://")) ? document.uri : `file://${document.uri}`;
 							// contents.push(`Symbol [${ans.symbolName}](${uri}#L${document.position.line + 1}) is overloaded: ${definitions.length} definitions found.`);
 							contents.push(`Symbol ${ans.symbolName} is overloaded: ${definitions.length} definitions found.`);
 							contents.push(`This tooltip shows one definition. Use peek-definition to view all definitions.`);
 						}	
-						if (def.symbolTheory && def.symbolDeclaration) {
-							if (def.symbolDeclarationRange && def.symbolDeclarationFile) {
-								const fileName: string = fsUtils.getFileName(def.symbolDeclarationFile);
+						if (def?.symbolTheory && def?.symbolDeclaration) {
+							if (def?.symbolDeclarationRange && def?.symbolDeclarationFile) {
+								const fileName: string = fsUtils.getFileName(def?.symbolDeclarationFile);
 								const link: MarkedString = // encoded as a markdown string
 									`[${fileName} (Ln ${def.symbolDeclarationRange.start.line}, Col ${def.symbolDeclarationRange.start.character})]`
-									+ `(file://${def.symbolDeclarationFile}`
-									+ `#L${def.symbolDeclarationRange.start.line})`;
+									+ `(file://${def.symbolDeclarationFile}#L${def.symbolDeclarationRange.start.line})`;
 									// + ", Col " + desc.symbolDeclarationRange.start.character + ")";
 								contents.push(link);
 							}
 							const content: MarkedString = {
-								value: def.symbolDeclaration,
+								value: def?.symbolDeclaration,
 								language: "pvs"
 							};
 							contents.push(content);
 						}
 					}
-				} else {
-					const link: MarkedString = // encoded as a markdown string
-							`[${ans.symbolName}](file://${desc.uri}#L${desc.position.line + 1})`;
-					contents.push(`No definition found for ${link}`);
 				}
 			}
+			// if not definitions was found, say so in the tooltip
+			if (contents?.length === 0 && ans?.symbolName) {
+				const link: MarkedString = `No definition found for [${ans.symbolName}](file://${desc.uri}#L${desc.position.line + 1})`;
+				contents.push(link);
+			}
 			return {
-				contents: contents,
+				contents,
 				range: { start: desc.position, end: desc.position } // the hover is located at the mouse position
 			};
 		}
